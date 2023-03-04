f8cc716f556356b5e86d525631eaf1b8d55f369e
JuanCamiloSalas <juansalasprogrammer@gmail.com>
2023-03-04 18:40:57 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+// Basic types
+let myTypeString: string = "hello world";
+let myTypeNumber: number = 33;
+let myTypeBoolean: boolean = true;
+
+// Array
+let arrNumber : number[] = [1,2,3];
+let arrNumber2 : Array<number> = [1,2,3];
+let arrStrings : string[] = ["uno", "dos", "tres"];
+let arrAny : any[] = ["Hole", 1, true, ()=>{}, {}];
+
+// Tuple
+let tuplePlayers: [string, number, boolean] = ["Killer", 25, true];
+
+// Tuple Array
+let players: [number, string][];
+players = [
+    [1, 'Lebron'],
+    [2, 'Jordan'],
+    [3, 'Carry'],
+]
+
+let myVariable;
+
+let myVariable1: boolean;
+
+let myVariable2 = "Hola mundo";
