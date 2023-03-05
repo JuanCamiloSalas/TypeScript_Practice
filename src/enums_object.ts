9c1c61be75e645a7165d0886f860175e9e99408c
JuanCamiloSalas <juansalasprogrammer@gmail.com>
2023-03-05 12:30:11 -0500

    feat: :rocket: Enums y objetos

---
+++ src/enums_object.ts
@@ -0,0 +1,15 @@
+// Enum
+enum Roles {
+    User = "USER",
+    Admin = "ADMIN",
+    SuperAdmin = "SUPERADMIN"
+}
+
+// Objects
+const roles = {
+    User: 0,
+    Admin: 1,
+    SuperAdmin: 2
+}
+
+console.log(roles.User);
